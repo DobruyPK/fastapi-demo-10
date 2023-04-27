e09b12c3de0c5fb2c4dada3eeda786bd7b533886
DobruyPK <65114650+DobruyPK@users.noreply.github.com>
2023-04-27 10:53:21 +0300

    testfirst

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Header
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -9,11 +8,9 @@ app = FastAPI()
 
 @app.get('/example')
 def example_get(CONTAINER_ID: str = Header()):
-    # Использование значения из хедера
-    return {'CONTAINER_ID': CONTAINER_ID}
+    return {'CONTAINER_ID': CONTAINER_ID, 'data': 'Im Container 1'}
 
 
 @app.post('/example')
 def example_post(CONTAINER_ID: str = Header()):
-    # Использование значения из хедера
-    return {'CONTAINER_ID': CONTAINER_ID}
+    return {'CONTAINER_ID': CONTAINER_ID, 'data': 'Im Container 1'}
