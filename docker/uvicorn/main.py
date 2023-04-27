8b7bc19452a8941591c4b981050265921c2b8dc0
DobruyPK <65114650+DobruyPK@users.noreply.github.com>
2023-04-27 18:39:25 +0300

    zcdsd

--- docker/uvicorn/main.py
+++ docker/uvicorn/main.py
@@ -8,9 +8,9 @@ app = FastAPI()
 
 @app.get('/example')
 def example_get(CONTAINER_ID: str = Header()):
-    return {'CONTAINER_ID': CONTAINER_ID, 'data': 'Im Container 3'}
+    return {'CONTAINER_ID': CONTAINER_ID, 'data': 'Im Container 1'}
 
 
 @app.post('/example')
 def example_post(CONTAINER_ID: str = Header()):
-    return {'CONTAINER_ID': CONTAINER_ID, 'data': 'Im Container 3'}
+    return {'CONTAINER_ID': CONTAINER_ID, 'data': 'Im Container 1'}
